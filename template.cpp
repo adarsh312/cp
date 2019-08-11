89b1300a8d14672f87b1dc0828b88d0b7c536931
adarsh ravichandran <adarshravichandran91@gmail.com>
2019-08-11 18:24:45 +0530

    template: adding test

--- template.cpp
+++ template.cpp
@@ -4,6 +4,8 @@
 #define rloop(i,a,b) for(int i=(int)a;i<=(int)b;++i)
 #define loopl(i,a,b) for(ll i=(ll)a;i<(ll)b;++i)
 #define get(a) cin>>a
+#define print(a) cout<<a<<endl
+#define sq(a) a*a
 #define getArray(length,a) for(int i=0;i<(int)length;++i) cin>>a[i];
 #define printArray(length,a) for(int i=0;i<(int)length;++i) cout<<a[i];
 
@@ -11,9 +13,9 @@ using namespace std;
 template <typename T> inline T maxi(T x, T y) { if(x>y) return x; else return y;}
 
 int main() {
-	// your code goes her
+	// your code goes 
 	test {
+	
 	}
 	return 0;
 }
-
