1aa850f47eab99ceb0707d9620445f9034bbf10f
adarsh ravichandran <adarshravichandran91@gmail.com>
2019-08-15 08:38:35 +0530

    template : adding mod and inf

--- template.cpp
+++ template.cpp
@@ -1,21 +1,23 @@
+
 #include <iostream>
 #define test int t;cin>>t;while(t--)
 #define loop(a,b) for(int i=(int)a;i<(int)b;++i)
-#define rloop(i,a,b) for(int i=(int)a;i<=(int)b;++i)
+#define rloop(a,b) for(int i=(int)a;i<=(int)b;++i)
 #define loopl(i,a,b) for(ll i=(ll)a;i<(ll)b;++i)
 #define get(a) cin>>a
 #define print(a) cout<<a<<endl
 #define sq(a) a*a
 #define getArray(length,a) for(int i=0;i<(int)length;++i) cin>>a[i];
 #define printArray(length,a) for(int i=0;i<(int)length;++i) cout<<a[i];
-
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
 using namespace std;
 template <typename T> inline T maxi(T x, T y) { if(x>y) return x; else return y;}
 
 int main() {
 	// your code goes 
+   
 	test {
-	
+	   
 	}
 	return 0;
 }
