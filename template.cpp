5b341cab7c74ebc3f63d56b228147f9eeb42892a
adarsh ravichandran <adarshravichandran@adarshs-MacBook-Pro.local>
2020-04-25 14:12:24 +0530

    template: adding the latest template

--- template.cpp
+++ template.cpp
@@ -1,21 +1,23 @@
-
-#include <iostream>
+#include <bits/stdc++.h>
 #define test int t;cin>>t;while(t--)
-#define loop(a,b) for(int i=(int)a;i<(int)b;++i)
-#define rloop(i,a,b) for(int i=(int)a;i<=(int)b;++i)
-#define loopl(i,a,b) for(ll i=(ll)a;i<(ll)b;++i)
-#define get(a) cin>>a
-#define print(a) cout<<a<<endl
-#define sq(a) a*a
-#define getArray(length,a) for(int j=0;j<(int)length;++j) cin>>a[j];
-#define printArray(length,a) for(int j=0;j<(int)length;++j) cout<<a[j]<<endl;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+//macros
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define SQ(a) a*a
+
 
 using namespace std;
-template <typename T> inline T maxi(T x, T y) { if(x>y) return x; else return y;}
 
 int main() {
 	test {
 	}
 	return 0;
 }
-
